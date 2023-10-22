5135f563458bcc68aace4efe5be12e0a38d5895d
dani <danielavargashidalgo@gmail.com>
2023-10-21 19:41:29 -0500

    variables de entorno

--- API/src/main/java/com/TBTB_Global/API/course/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TBTB_Global.API.course.repository;
-
-import com.TBTB_Global.API.course.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
